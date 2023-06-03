452f9e88b7a59d8d8d16fdf0760fcbb51215e60f
ANHTUANPHUNG <132957644+ANHTUANPHUNG@users.noreply.github.com>
2023-06-03 21:53:31 +0700

    update

--- tieman/data.js
+++ tieman/data.js
@@ -67,7 +67,7 @@ let html = ``
 listData.map((product, index) => {
     return html += `
     <div class="item">
-            <img src="${product.image}" alt="">
+            <img src="${product.image}" class ="img" alt="">
             <div class="stars">
             <span>
             <img src="fileanh/star.png" alt="">
